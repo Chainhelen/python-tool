af5fb2b14ee43dfa1b21a37ef863b9c72085dc5d
chainhelen <chainhelen@gmail.com>
2015-04-24 08:29:39 -0400

    code2

--- main.py
+++ main.py
@@ -9,14 +9,29 @@ sys.setdefaultencoding('utf-8')
 
 #Gets
 headers =  header.getHeader("headerdata")
-url = "https://www.hackthis.co.uk/levels/coding/1"
+url = "https://www.hackthis.co.uk/levels/coding/2"
 r = requests.get(url, headers = headers);
+
+def getChar(string):
+    ll = string.split(',');
+    ll = map(int, ll);
+    res = '';
+    for i in ll:
+        res += chr(158 - i)
+    return res
+
 
 #pyq process
 jq = pyq(r.text);
-li = jq("textarea").text().split(', ');
-li.sort();
-payload = {'answer':', '.join(li)};
+li = jq("textarea").text().split(', ,');
+res = [];
+
+for i in li:
+    res.append(getChar(i));
+
+#print ' '.join(res)
+#payload = {'answer':', '.join(li)};
+payload = {'answer':' '.join(res)};
 
 print payload
 #Post
